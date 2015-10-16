439d172f508d779f899ded9b04bac45da3a0d7f3
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-10-16 10:52:39 +0100

    Adds route taking in request params - i.e. using url query params

--- server.js
+++ server.js
@@ -8,5 +8,11 @@ app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
+app.get('/jedi/:firstname/:lastname', function(req, res) {
+  res.send(req.params.firstname);
+  // console.log(req.params.firstname); this makes the first name in the url query string pop up in the terminal console but not in the Chrome inspect element console.
+});
+
+
 app.listen(8080);
 console.log("App listening on port 8080"); 
